311a47bfe26b982b3db3576a694f8ad661e9b137
Sherlook <re.gonzalo@yahoo.com.ar>
2021-02-23 21:14:22 -0300

    Create the type service and index method

---
+++ src/app/services/type.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TypeService {
+  base_url: string;
+
+  constructor(private http: HttpClient) { 
+    this.base_url = environment.api_url
+  }
+
+  getTypes() {
+    return this.http.get(`${this.base_url}/type/index}`);
+  }
+}
