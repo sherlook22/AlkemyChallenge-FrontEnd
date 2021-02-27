a12413dda442c51fdf99aed371d77eeff9f7fc94
Sherlook <re.gonzalo@yahoo.com.ar>
2021-02-27 12:51:24 -0300

    Created the register form and validate passwords

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.registerForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      password2: ['', Validators.required]
+    }, {
+      validators: this.equalsPasswords()
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  equalsPasswords() {
+    return ( formGroup: FormGroup ) => {
+      const pass1: any = formGroup.get('password');
+      const pass2: any = formGroup.get('password2');
+      if( pass1.value !== pass2.value ) pass2?.setErrors({ equal: false });
+    }
+  }
+
+  registerAccion() {
+    Object.values( this.registerForm.controls).forEach((ctls) => {
+      ctls.markAsTouched();
+    })
+  }
+
+  validateData(){
+    console.log(this.registerForm.valid);
+    console.log(this.registerForm.value);
+    this.registerAccion()
+    
   }
 
 }
